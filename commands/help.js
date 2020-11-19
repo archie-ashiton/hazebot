01290e2c86e89344d70227fd9f37a2542f144fb2
archie-ashiton <74156930+archie-ashiton@users.noreply.github.com>
2020-11-19 08:39:01 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,9 @@ module.exports = {
             color: 0xBCB7AB,
             title: 'haze',
             description: '<> - optional arguments\n[] - mandatory arguments\n\n**General Commands**\n`,help` - displays this menu\n\n**Fun Commands**\n `,rps` - rock, paper, scissors\n\n**Staff Commands**\n`,role [role] [user]`',
+            thumbnail: {
+                url: 'https://media.discordapp.net/attachments/778754863516483584/778977513912533002/beigeaesthetic.jpg'
+              },
             footer: {
                 text: 'xoxo',
             },
