e68b1e1d04b6ffd53abff0e3c8fce83c4d3b473a
archie-ashiton <74156930+archie-ashiton@users.noreply.github.com>
2020-11-19 08:41:42 -0500

    upload

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ module.exports = {
         const helpEmbed = {
             color: 0xBCB7AB,
             title: 'haze',
-            description: '<> - optional arguments\n[] - mandatory arguments\n\n**General Commands**\n`,help` - displays this menu\n\n**Fun Commands**\n `,rps` - rock, paper, scissors\n\n**Staff Commands**\n`,role [role] [user]`',
+            description: '<> = optional arguments\n[] = mandatory arguments\n\n**General Commands**\n`,help` - displays this menu\n\n**Fun Commands**\n `,rps` - rock, paper, scissors\n\n**Staff Commands**\n`,role [role] [user]`',
             thumbnail: {
                 url: 'https://media.discordapp.net/attachments/778754863516483584/778977513912533002/beigeaesthetic.jpg'
               },
--- commands/rps.js
+++ commands/rps.js
@@ -5,27 +5,27 @@ module.exports = {
         let number = Math.floor(Math.random() * 3) + 1;
         if(number === 1){
             if(args[0] === 'rock'){
-                message.channel.send('I pick rock, it\s a tie!');
+                message.channel.send('I pick `rock`, it\s a tie!');
             }else if(args[0] === 'paper'){
-                message.channel.send('I pick rock, you win!');
+                message.channel.send('I pick `rock`, you win!');
             }else if(args[0] === 'scissors'){
-                message.channel.send('I pick rock, I win!');
+                message.channel.send('I pick `rock`, I win!');
             }
         }else if(number === 2){
             if(args[0] === 'rock'){
-                message.channel.send('I pick paper, I win!');
+                message.channel.send('I pick `paper`, I win!');
             }else if(args[0] === 'paper'){
-                message.channel.send('I pick paper, it\'s a tie!');
+                message.channel.send('I pick `paper`, it\'s a tie!');
             }else if(args[0] === 'scissors'){
-                message.channel.send('I pick paper, you win!');
+                message.channel.send('I pick `paper`, you win!');
             }
         }else if(number === 3){
             if(args[0] === 'rock'){
-                message.channel.send('I pick scissors, you win!');
+                message.channel.send('I pick `scissors`, you win!');
             }else if(args[0] === 'paper'){
-                message.channel.send('I pick scissors, I win!');
+                message.channel.send('I pick `scissors`, I win!');
             }else if(args[0] === 'scissors'){
-                message.channel.send('I pick scissors, it\'s a tie!');
+                message.channel.send('I pick `scissors`, it\'s a tie!');
             }
         }
     }
